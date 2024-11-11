70403d1e0f71d9d64bc46bb91ed566ccbae85a30
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-11-11 11:08:19 +0530

    Added personal portfolio app in the project

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
+    "pages.apps.PagesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / "templates",
+            BASE_DIR / "templates/",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
