b9cd6e0a17e2a13729de092348a9533530ccbdc1
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-11-13 11:19:43 +0530

    Register/Create a new user

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
 # django.contrib.auth.urls will provide access to the default login and logout views
--- users/views.py
+++ users/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
 
 def dashboard(request):
     return render(request, "users/dashboard.html")
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "users/register.html", {"form": UserCreationForm()})
+
+    elif request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("dashboard")
